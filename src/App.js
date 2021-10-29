3ba8f8d51fc8be312bba5c3e7d772f9ecc2c6c5f
Ariel P <78217478+ArielPMx@users.noreply.github.com>
2021-10-29 18:32:07 -0400

    Prueba el pipeline etiqueta <a>

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          El PIPELINE FUNCIONA
         </a>
       </header>
     </div>
